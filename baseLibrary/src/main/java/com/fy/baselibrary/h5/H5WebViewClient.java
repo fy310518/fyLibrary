a8b5afb8fdca54ddb069551989ae159ee0bb0b98
fy310518 <fy310518@163.com>
2020-03-06 00:14:56 +0800

    动态 多状态布局

--- baseLibrary/src/main/java/com/fy/baselibrary/h5/H5WebViewClient.java
+++ baseLibrary/src/main/java/com/fy/baselibrary/h5/H5WebViewClient.java
@@ -64,12 +64,12 @@ public class H5WebViewClient extends WebViewClient {
     @Override
     public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
         super.onReceivedError(view, errorCode, description, failingUrl);
+        view.loadUrl("about:blank"); // 避免出现默认的错误界面
         // 断网或者网络连接超时
         if (errorCode == ERROR_HOST_LOOKUP || errorCode == ERROR_CONNECT || errorCode == ERROR_TIMEOUT) {
-            view.loadUrl("about:blank"); // 避免出现默认的错误界面
-            if (null != onSetStatusView) {
-                onSetStatusView.showHideViewFlag(Constant.LAYOUT_NETWORK_ERROR_ID);
-            }
+            setTips(Constant.LAYOUT_NETWORK_ERROR_ID);
+        } else {
+            setTips(Constant.LAYOUT_ERROR_ID);
         }
     }
 
@@ -78,12 +78,18 @@ public class H5WebViewClient extends WebViewClient {
     public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
         super.onReceivedHttpError(view, request, errorResponse);
 
+        view.loadUrl("about:blank");// 避免出现默认的错误界面
         int statusCode = errorResponse.getStatusCode();
         if (404 == statusCode || 500 == statusCode) {
-            view.loadUrl("about:blank");// 避免出现默认的错误界面
-            if (null != onSetStatusView) {
-                onSetStatusView.showHideViewFlag(Constant.LAYOUT_ERROR_ID);
-            }
+            setTips(Constant.LAYOUT_ERROR_ID);
+        } else {
+            setTips(Constant.LAYOUT_NETWORK_ERROR_ID);
+        }
+    }
+
+    private void setTips(int status){
+        if (null != onSetStatusView) {
+            onSetStatusView.showHideViewFlag(status);
         }
     }
 }
